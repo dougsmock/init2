650cb499744f1aff3b9de11f11d4be0290471341
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-20 10:53:11 -0400

    Add files via upload

---
+++ integers_add.rb
@@ -0,0 +1,21 @@
+
+def compare(a, b)
+  puts "2 numbers have been entered."
+  puts "#{a} == #{b}"
+  return a == b
+end
+
+compare_these = compare(9999, 7)
+  puts "#{compare_these}"
+
+
+
+
+
+
+
+  #write a function take two objects
+
+  #tell if same class
+
+  #if same class tell if same value.
